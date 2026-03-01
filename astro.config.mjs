75b16d1a7a02f49e4575f368bca1f82702457ba6
Kelly Mahlangu <kellymahlangu01@gmail.com>
2026-03-01 09:47:44 +0200

    chore: move apikey to .env & fix sitemap-index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+const isProd = process.env.VERCEL_ENV === "production";
+
 export default defineConfig({
+    site: "https://mahlogic.africa",
     vite: {
-        plugins: [tailwindcss(), sitemap()],
+        plugins: [tailwindcss()],
     },
+    integrations: [
+        // Only generate sitemap on production deployments
+        ...(isProd ? [sitemap()] : []),
+    ],
 });
